374682a08d8359d8effcf5a6a3afc128c4afe9b8
Kamil Markowski <kamil0markowski@gmail.com>
2024-08-14 23:30:43 +0200

    core types added

--- app.js
+++ app.js
@@ -1,1 +1,36 @@
-console.log("Your code goes here...");
+// const person: {
+//     name: string;
+//     age: number;
+// } = {
+//     name: 'Maximilian',
+//     age: 30
+// }
+// const person: {
+//     name: string;
+//     age: number;
+//     hobbies: string[];
+//     role: [number, string] //tuple type defined inside brackets
+// } = {
+//     name: 'Maximilian',
+//     age: 30,
+//     hobbies: ['Sports', 'Cooking'],
+//     role: [2, 'author']
+// };
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 0] = "ADMIN";
+    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
+    Role[Role["AUTHOR"] = 2] = "AUTHOR";
+})(Role || (Role = {}));
+;
+var person = {
+    name: 'Maximilian',
+    age: 30,
+    hobbies: ['Sports', 'Cooking'],
+    role: Role.ADMIN
+};
+// person.role.push('admin');
+// person.role[1] = 10;
+var favoriteActivities;
+favoriteActivities = ['Sports', 1];
+console.log(person);
--- app.ts
+++ app.ts
@@ -1,1 +1,36 @@
-console.log("Your code goes here...")
+// const person: {
+//     name: string;
+//     age: number;
+// } = {
+//     name: 'Maximilian',
+//     age: 30
+// }
+
+// const person: {
+//     name: string;
+//     age: number;
+//     hobbies: string[];
+//     role: [number, string] //tuple type defined inside brackets
+// } = {
+//     name: 'Maximilian',
+//     age: 30,
+//     hobbies: ['Sports', 'Cooking'],
+//     role: [2, 'author']
+// };
+
+enum Role { ADMIN, READ_ONLY, AUTHOR };
+
+const person = {
+    name: 'Maximilian',
+    age: 30,
+    hobbies: ['Sports', 'Cooking'],
+    role: Role.ADMIN
+};
+
+// person.role.push('admin');
+// person.role[1] = 10;
+
+let favoriteActivities: any[];
+favoriteActivities = ['Sports', 1]
+
+console.log(person)
