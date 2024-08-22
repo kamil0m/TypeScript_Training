ebcad3201d1ef8508be67ddfe873d51f016d5758
Kamil Markowski <kamil0markowski@gmail.com>
2024-08-22 12:07:26 +0200

    getters and setters added

--- src/app.ts
+++ src/app.ts
@@ -31,8 +31,25 @@ class ITDepartment extends Department {
 };
 
 class AccountingDepartment extends Department {
+    private lastReport: string;
+
+    get mostRecentReport() {
+        if (this.lastReport) {
+            return this.lastReport;
+        }
+        throw new Error('No report found.')
+    }
+
+    set mostRecentReport(value: string) {
+        if (!value) {
+            throw new Error('Please pass in a valid value!');
+        }
+        this.addReport(value);
+        }
+
     constructor(id: string,  private reports: string[]) {
         super(id, 'Accounting');
+        this.lastReport = reports[0];
     }
 
     addEmployee(name: string) {
@@ -44,6 +61,7 @@ class AccountingDepartment extends Department {
 
     addReport(text: string) {
         this.reports.push(text);
+        this.lastReport = text;
     }
     getReports() {
         console.log(this.reports)
@@ -63,6 +81,11 @@ console.log(it);
 
 
 const accounting =  new AccountingDepartment('d1', []);
+
+accounting.mostRecentReport = '';
+accounting.addReport('Something went wrong...');
+
+console.log(accounting.mostRecentReport);
 
 accounting.addEmployee('Max');
 accounting.addEmployee('Manu');
