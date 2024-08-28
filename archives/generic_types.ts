1a6db83ac6b1c7cd4d47f54e3f1ac8c3d19ac9fa
Kamil Markowski <kamil0markowski@gmail.com>
2024-08-28 12:05:40 +0200

    basic decorators added

---
+++ archives/generic_types.ts
@@ -0,0 +1,93 @@
+// const names: Array<string> = []; // is array of strings = string[]
+// //names [0].split(' ');
+
+// const promise: Promise<number> = new Promise(((resolve, reject) => {
+//     setTimeout(() => {
+//         resolve(10);
+//     }, 2000);
+// }))
+
+// promise.then(data => {
+//     data.split(' ');
+// })
+
+function merge<T extends object, U extends object>(objA: T, objB: U) { // T & U are generics informing TS that there wsill be two different types of objects
+    return Object.assign(objA, objB);
+}
+
+const mergedObj = merge({ name: 'Max', hobbies: ['Sports'] }, { age: 30 });
+console.log(mergedObj.age);
+
+interface Lengthy {
+    length: number;
+}
+
+function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
+    let descriptionText = 'Got no value.';
+    if (element.length === 1) {
+        descriptionText = 'Got 1 element.';
+    } else if (element.length > 0) {
+        descriptionText = 'Got ' + element.length + ' elements.';
+    }
+    return [element, descriptionText];
+}
+
+console.log(countAndDescribe('Hi there!'));
+console.log(countAndDescribe(['Sports', 'Cooking']));
+console.log(countAndDescribe([]));
+
+function extractAndConvert<T extends object, U extends keyof T>(obj: T, key: U) {
+    return obj[key];
+}
+
+console.log(extractAndConvert({ name: 'Max' }, 'name'));
+
+class DataStorage<T extends string | number | boolean> {
+    private data: T[] = [];
+
+    addItem(item: T) {
+        this.data.push(item);
+    }
+
+    removeItem(item: T) {
+        if (this.data.indexOf(item) === -1) {
+            return;
+        }
+        this.data.splice(this.data.indexOf(item), 1);
+    }
+
+    getItems() {
+        return [... this.data];
+    }
+}
+
+const textStorage = new DataStorage<string>();
+textStorage.addItem('Max');
+textStorage.addItem('Manu');
+textStorage.removeItem('Max');
+console.log(textStorage.getItems());
+
+const numberStorage = new DataStorage<number>();
+
+// const objStorage = new DataStorage<object>();
+// const maxObj = {name: 'Max'};
+// objStorage.addItem(maxObj);
+// objStorage.addItem({name: 'Manu'});
+// // ...
+// objStorage.removeItem(maxObj);
+// console.log(objStorage.getItems());
+
+interface CourseGoal {
+    title: string;
+    description: string;
+    completeUntil: Date;
+}
+
+// function createCourseGoal(
+//     title: string, 
+//     description: string, 
+//     date: Date
+// ): CourseGoal {
+//     let
+//     tit
+// }
--- src/app.ts
+++ src/app.ts
@@ -1,78 +1,66 @@
-// const names: Array<string> = []; // is array of strings = string[]
-// //names [0].split(' ');
-
-// const promise: Promise<number> = new Promise(((resolve, reject) => {
-//     setTimeout(() => {
-//         resolve(10);
-//     }, 2000);
-// }))
-
-// promise.then(data => {
-//     data.split(' ');
-// })
-
-function merge<T extends object, U extends object>(objA: T, objB: U) { // T & U are generics informing TS that there wsill be two different types of objects
-    return Object.assign(objA, objB);
-}
-
-const mergedObj = merge({ name: 'Max', hobbies: ['Sports'] }, { age: 30 });
-console.log(mergedObj.age);
-
-interface Lengthy {
-    length: number;
-}
-
-function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
-    let descriptionText = 'Got no value.';
-    if (element.length === 1) {
-        descriptionText = 'Got 1 element.';
-    } else if (element.length > 0) {
-        descriptionText = 'Got ' + element.length + ' elements.';
+function Logger(logString: string) {
+    console.log('LOGGER FACTORY');
+    return function (constructor: Function) {
+        console.log(logString);
+        console.log(constructor);
     }
-    return [element, descriptionText];
 }
 
-console.log(countAndDescribe('Hi there!'));
-console.log(countAndDescribe(['Sports', 'Cooking']));
-console.log(countAndDescribe([]));
-
-function extractAndConvert<T extends object, U extends keyof T>(obj: T, key: U) {
-    return obj[key];
-}
-
-console.log(extractAndConvert({ name: 'Max' }, 'name'));
-
-class DataStorage<T extends string | number | boolean> {
-    private data: T[] = [];
-
-    addItem(item: T) {
-        this.data.push(item);
-    }
-
-    removeItem(item: T) {
-        if (this.data.indexOf(item) === -1) {
-            return;
+function WithTemplate(template: string, hookId: string) {
+    console.log('TEMPLATE FACTORY');
+    return function(constructor: any) {
+        console.log('Rendering template');
+        const hookEl = document.getElementById(hookId);
+        const p = new constructor
+        if (hookEl) {
+            hookEl.innerHTML = template;
+            hookEl.querySelector('h1')!.textContent = p.name
         }
-        this.data.splice(this.data.indexOf(item), 1);
-    }
-
-    getItems() {
-        return [... this.data];
     }
 }
 
-const textStorage = new DataStorage<string>();
-textStorage.addItem('Max');
-textStorage.addItem('Manu');
-textStorage.removeItem('Max');
-console.log(textStorage.getItems());
 
-const numberStorage = new DataStorage<number>();
+// @Logger('LOGGING - PERSON')
+@Logger('LOGGING')
+@WithTemplate('<h1>My Person object</h1>', 'app')
+class Person {
+    name = 'Max'
 
-const objStorage = new DataStorage<object>();
-const maxObj = {name: 'Max'};
-objStorage.addItem(maxObj);
-objStorage.addItem({name: 'Manu'});
-// ...
-objStorage.removeItem(maxObj);
-console.log(objStorage.getItems());
+    constructor(){
+        console.log('Creating person from object...')
+    }
+}
+
+const pers = new Person();
+
+console.log(pers);
+
+// ---
+
+function Log(target: any, propertyName: string | Symbol) {
+    console.log('Property decorator!');
+    console.log(target, propertyName);
+}
+
+class Product {
+    @Log
+    title: string;
+    private _price: number;
+    
+    set price(val: number) {
+        if (val > 0) {
+            this._price = val;
+        } else {
+            throw new Error('Invalid price - should be positive!');
+        }
+    }
+
+    constructor(t: string, p: number) {
+        this.title = t;
+        this._price = p;
+    }
+
+    getPriceWithTax(tax: number) {
+        return this._price * (1 + tax);
+    }
+}
